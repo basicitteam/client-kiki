a0814fbcfb590ad4c08b3a13ca7cd20845307912
Rogers Dwiputra <setiady.rogers28@gmail.com>
2012-11-25 11:30:35 +0700

    view buat di admin saat set siswa mengambil matkul, belum jalan

---
+++ application/views/admin/set_siswa.php
@@ -0,0 +1,43 @@
+
+					<?php echo $this->session->flashdata('msg'); ?>
+					<div class="span9 well">
+						<div class="alert alert-info">
+							<form class="form-search" style="padding-top:15px" action="<?php echo site_url('admin/add_set_siswa'); ?>" method="POST" enctype="multipart/form-data">
+							  <input type="text" placeholder="Input NIS" class="input-medium search-query">
+								<button type="submit" class="btn">Search</button>
+							
+								<div class="btn-group pull-right">
+								  <button class="btn btn-primary">Tambah Mata Pelajaran</button>
+								  <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
+								  <ul class="dropdown-menu">
+									<li><a href="<?php echo site_url('admin/add_set_siswa_manual'); ?>">Manual</a></li>
+								  </ul>
+								</div>
+							</form>
+							<table class="table table-striped">
+								<thead>
+								  <tr>
+									<th><center>NIS</center></th>
+									<th><center>Nama Siswa</center></th>
+									<th><center>Mata Pelajaran</center></th>
+								  </tr>
+								</thead>
+								<tbody>
+								</tbody>
+							  </table>
+							  <div class="pagination">
+									<center>
+										<ul>
+										  <li><a href="#">&laquo;</a></li>
+										  <li><a href="#">1</a></li>
+										  <li><a href="#">2</a></li>
+										  <li><a href="#">3</a></li>
+										  <li><a href="#">4</a></li>
+										  <li><a href="#">&raquo;</a></li>
+										</ul>
+									</center>
+							  <?php echo $this->pagination->create_links(); ?>
+						</div>
+					</div>
+					
+				
